5a9fa26b7711743c06cc87fd82d61410d02ec2d9
Vamshi Poreddy <80277493+vamshi-poreddy@users.noreply.github.com>
2024-01-04 18:36:48 +0000

    changes

--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp1
+namespace Note_taking
 {
     public partial class Form1 : Form
     {
@@ -27,7 +27,6 @@ namespace WindowsFormsApp1
             SavedNotes.DataSource = notes;
             SavedNotes.Columns["Message"].Visible = false;
             SavedNotes.Columns["Title"].Width = 210;
-            SavedNotes.Columns["Title"].
         }
 
 
