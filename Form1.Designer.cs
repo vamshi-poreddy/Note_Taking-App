a84d51796ad4b4aab23ea2b8a36dda490380cd95
Vamshi Poreddy <80277493+vamshi-poreddy@users.noreply.github.com>
2024-01-04 18:19:19 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textTitle = new System.Windows.Forms.TextBox();
+            this.textMsg = new System.Windows.Forms.TextBox();
+            this.SavedNotes = new System.Windows.Forms.DataGridView();
+            this.btNew = new System.Windows.Forms.Button();
+            this.btSave = new System.Windows.Forms.Button();
+            this.btRead = new System.Windows.Forms.Button();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.SavedNotes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Location = new System.Drawing.Point(50, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Message:";
+            this.label2.UseWaitCursor = true;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textTitle
+            // 
+            this.textTitle.Location = new System.Drawing.Point(111, 51);
+            this.textTitle.Name = "textTitle";
+            this.textTitle.Size = new System.Drawing.Size(282, 22);
+            this.textTitle.TabIndex = 2;
+            // 
+            // textMsg
+            // 
+            this.textMsg.Location = new System.Drawing.Point(114, 94);
+            this.textMsg.Multiline = true;
+            this.textMsg.Name = "textMsg";
+            this.textMsg.Size = new System.Drawing.Size(288, 250);
+            this.textMsg.TabIndex = 3;
+            // 
+            // SavedNotes
+            // 
+            this.SavedNotes.AllowUserToAddRows = false;
+            this.SavedNotes.AllowUserToDeleteRows = false;
+            this.SavedNotes.AllowUserToResizeColumns = false;
+            this.SavedNotes.AllowUserToResizeRows = false;
+            this.SavedNotes.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
+            this.SavedNotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SavedNotes.Location = new System.Drawing.Point(443, 59);
+            this.SavedNotes.Name = "SavedNotes";
+            this.SavedNotes.ReadOnly = true;
+            this.SavedNotes.RowHeadersVisible = false;
+            this.SavedNotes.RowHeadersWidth = 51;
+            this.SavedNotes.RowTemplate.Height = 24;
+            this.SavedNotes.RowTemplate.ReadOnly = true;
+            this.SavedNotes.ShowCellErrors = false;
+            this.SavedNotes.Size = new System.Drawing.Size(285, 285);
+            this.SavedNotes.TabIndex = 4;
+            this.SavedNotes.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.SavedNotes_CellContentDoubleClick);
+            // 
+            // btNew
+            // 
+            this.btNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btNew.Location = new System.Drawing.Point(60, 366);
+            this.btNew.Name = "btNew";
+            this.btNew.Size = new System.Drawing.Size(138, 35);
+            this.btNew.TabIndex = 5;
+            this.btNew.Text = "New";
+            this.btNew.UseVisualStyleBackColor = true;
+            this.btNew.Click += new System.EventHandler(this.btNew_Click);
+            // 
+            // btSave
+            // 
+            this.btSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSave.Location = new System.Drawing.Point(246, 366);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(147, 35);
+            this.btSave.TabIndex = 6;
+            this.btSave.Text = "Save Note";
+            this.btSave.UseVisualStyleBackColor = true;
+            this.btSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // btRead
+            // 
+            this.btRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btRead.Location = new System.Drawing.Point(431, 366);
+            this.btRead.Name = "btRead";
+            this.btRead.Size = new System.Drawing.Size(130, 35);
+            this.btRead.TabIndex = 7;
+            this.btRead.Text = "Display";
+            this.btRead.UseVisualStyleBackColor = true;
+            this.btRead.Click += new System.EventHandler(this.btRead_Click);
+            // 
+            // btDelete
+            // 
+            this.btDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDelete.Location = new System.Drawing.Point(610, 366);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(144, 35);
+            this.btDelete.TabIndex = 8;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(511, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Saved Notes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(782, 442);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.btRead);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.btNew);
+            this.Controls.Add(this.SavedNotes);
+            this.Controls.Add(this.textMsg);
+            this.Controls.Add(this.textTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Note Taking Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.SavedNotes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textTitle;
+        private System.Windows.Forms.TextBox textMsg;
+        private System.Windows.Forms.DataGridView SavedNotes;
+        private System.Windows.Forms.Button btNew;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Button btRead;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        DataTable notes = new DataTable();
+        bool editing=  false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notes.Columns.Add("Title");
+            notes.Columns.Add("Message");
+
+            SavedNotes.DataSource = notes;
+            SavedNotes.Columns["Message"].Visible = false;
+            SavedNotes.Columns["Title"].Width = 210;
+            SavedNotes.Columns["Title"].
+        }
+
+
+        private void btNew_Click(object sender, EventArgs e)
+        {
+            textTitle.Text = "";
+            textMsg.Text = "";
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                notes.Rows[SavedNotes.CurrentCell.RowIndex].Delete();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Select a Valid Entry");
+            }
+        }
+
+        private void btRead_Click(object sender, EventArgs e)
+        {
+            textTitle.Text = notes.Rows[SavedNotes.CurrentCell.RowIndex].ItemArray[0].ToString();
+            textMsg.Text = notes.Rows[SavedNotes.CurrentCell.RowIndex].ItemArray[1].ToString();
+
+            editing = true;
+        }
+
+        private void btSave_Click(object sender, EventArgs e)
+        {
+            if (editing)
+            {
+                notes.Rows[SavedNotes.CurrentCell.RowIndex]["Title"] = textTitle.Text;
+                notes.Rows[SavedNotes.CurrentCell.RowIndex]["Message"] = textMsg.Text;
+            }
+            else
+            {
+                Console.WriteLine(textTitle.Text.ToString());
+                Console.WriteLine(textMsg.Text.ToString());
+                var f = textTitle.Text.ToString() != "" ? textTitle.Text.ToString() : "No Title";
+                notes.Rows.Add(f, textMsg.Text);
+            }
+            textTitle.Text="";
+            textMsg.Text="";
+            editing= false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SavedNotes_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textTitle.Text = notes.Rows[SavedNotes.CurrentCell.RowIndex].ItemArray[0].ToString();
+            textMsg.Text = notes.Rows[SavedNotes.CurrentCell.RowIndex].ItemArray[1].ToString();
+
+            editing = true;
+        }
+    }
+}
